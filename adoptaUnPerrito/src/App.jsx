aa52e3cc19f86e4e8bad1018e272fe0dee32de9e
sarebok <bortonae@hotmail.com>
2023-10-01 18:29:19 -0300

    install normalize and boostrap

--- adoptaUnPerrito/src/App.jsx
+++ adoptaUnPerrito/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Footer></Footer>
     </>
   );
 }
---
+++ adoptaUnPerrito/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>Generar un componente Footer.jsx que muestre el pie de página o footer con una breve descripción de la galería de imágenes.</p>
+      </footer>
+      ;
+    </>
+  );
+};
+
+export default Footer;
